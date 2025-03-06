56d7e16d4c8c85c2f75afce901310d495496f8a9
ROHIT_0906 <rohitmakam09@gmail.com>
2025-03-06 20:21:13 +0530

    login-page

---
+++ script.js
@@ -0,0 +1,6 @@
+let loginBtn = document.querySelector(".login-btn");
+let wraper = document.querySelector(".wraper");
+
+loginBtn.addEventListener("click", function(){
+wraper.classList.add("active");
+});
