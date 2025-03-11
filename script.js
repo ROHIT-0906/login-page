01223c506031d0e9031c6101d720c3da019b4be9
ROHIT_0906 <rohitmakam09@gmail.com>
2025-03-11 17:32:31 +0530

    add new profile feature

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ let wraper = document.querySelector(".wraper");
 loginBtn.addEventListener("click", function(){
 wraper.classList.add("active");
 });
+
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profilePic').src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
